d88bb56d681e07d2eab0ee497bad483d8e8e9ea0
agung <agungnugraha180405@gmail.com>
2025-10-20 14:01:07 +0700

    add files

--- Belajar_pengembangan_web_intermediate/Transisi_dan_animasi/spa-with-mvp-starter-project/scripts/app.js
+++ Belajar_pengembangan_web_intermediate/Transisi_dan_animasi/spa-with-mvp-starter-project/scripts/app.js
@@ -1,5 +1,5 @@
-import { getActiveRoute } from './routes/url-parser.js';
-import routes from './routes/routes.js';
+import { getActiveRoute } from "./routes/url-parser.js";
+import routes from "./routes/routes.js";
 
 export default class App {
   #content;
@@ -16,5 +16,6 @@ export default class App {
     const page = route();
 
     this.#content.innerHTML = await page.render();
+    await page.afterRender();
   }
 }
--- Belajar_pengembangan_web_intermediate/Transisi_dan_animasi/spa-with-mvp-starter-project/scripts/data/local/cats.js
+++ Belajar_pengembangan_web_intermediate/Transisi_dan_animasi/spa-with-mvp-starter-project/scripts/data/local/cats.js
@@ -1,29 +1,9 @@
-let cats = [
-  {
-    id: 1,
-    name: 'Cici',
-    image: '/images/01.jpg',
-  },
-  {
-    id: 2,
-    name: 'Milo',
-    image: '/images/02.jpg',
-  },
-  {
-    id: 3,
-    name: 'Simba',
-    image: '/images/03.jpg',
-  },
-];
-
-const CatsLocal = {
+export const CatsLocal = {
   async getAllCats() {
-    // TODO: Implementation
+    return [...cats];
   },
 
   async getCatById(id) {
     // TODO: Implementation
   },
 };
-
-export default CatsLocal;
